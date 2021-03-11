2c97c216ae45816e43f9ee2c1640a05926f25ad4
Armand Bautista <agb7582@yahoo.com>
2021-03-11 13:32:39 -0800

    Presentational and Container Components

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,54 +1,61 @@
 import React, { Component } from "react";
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import {
+  Card,
+  CardImg,
+  CardImgOverlay,
+  CardText,
+  CardBody,
+  CardTitle,
+} from "reactstrap";
 
 class CampsiteInfo extends Component {
   constructor(props) {
     super(props);
   }
-  
-  renderCampsite(campsite){
-    return(
+
+  renderCampsite(campsite) {
+    return (
       <div className="col-md-5 m-1">
         <Card>
-            <CardImg top src={campsite.image} alt={campsite.name} />
-            <CardBody>
-              <CardTitle>{campsite.name}</CardTitle>
-              <CardText>{campsite.description}</CardText>
-            </CardBody>
+          <CardImg top src={campsite.image} alt={campsite.name} />
+          <CardBody>
+            <CardTitle>{campsite.name}</CardTitle>
+            <CardText>{campsite.description}</CardText>
+          </CardBody>
         </Card>
       </div>
     );
   }
 
-  renderComments(comments){
-    if (comments){
+  renderComments(comments) {
+    if (comments) {
       return (
         <div className="col-md-5 m-1">
           <h4>Comments</h4>
-          {comments.map(comment => 
-            <div style={{marginBottom:10}} key={comment.id}> 
-                <div>{comment.text}</div>
-                <div>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { 
-                  year: 'numeric',
-                  month: 'short', 
-                  day: '2-digit'
-                  }).format(new Date(Date.parse(comment.date)))}
-                </div>
-                
-            </div> ) 
-
-          }
+          {comments.map((comment) => (
+            <div style={{ marginBottom: 10 }} key={comment.id}>
+              <div>{comment.text}</div>
+              <div>
+                -- {comment.author},{" "}
+                {new Intl.DateTimeFormat("en-US", {
+                  year: "numeric",
+                  month: "short",
+                  day: "2-digit",
+                }).format(new Date(Date.parse(comment.date)))}
+              </div>
+            </div>
+          ))}
         </div>
       );
     }
-    return <div/>;
-  }  
-  
+    return <div />;
+  }
+
   render() {
-    const {campsite} = this.props;
+    const { campsite } = this.props;
     if (campsite) {
       return (
-        <div>
+        <div className="container">
           <div className="row">
             {this.renderCampsite(campsite)}
             {this.renderComments(campsite.comments)}
@@ -58,8 +65,6 @@ class CampsiteInfo extends Component {
     }
     return <div />;
   }
-  
 }
-
 
 export default CampsiteInfo;
