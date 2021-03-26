2e0867bd60a54e42ff88af5bac9e1c741d793236
Armand Bautista <agb7582@yahoo.com>
2021-03-26 16:31:35 -0700

    Fetch Post Comment

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,8 +8,13 @@ import About from "./AboutComponent";
 import Contact from "./ContactComponent";
 import { Switch, Route, Redirect, withRouter } from "react-router-dom";
 import { connect } from "react-redux";
-import { actions } from 'react-redux-form';
-import { addComment, fetchCampsites, fetchComments, fetchPromotions } from '../redux/ActionCreators';
+import { actions } from "react-redux-form";
+import {
+  postComment,
+  fetchCampsites,
+  fetchComments,
+  fetchPromotions,
+} from "../redux/ActionCreators";
 
 const mapStateToProps = (state) => {
   return {
@@ -21,32 +26,36 @@ const mapStateToProps = (state) => {
 };
 
 const mapDispatchToProps = {
-  addComment: (campsiteId, rating, author, text) => (addComment(campsiteId, rating, author, text)),
-  fetchCampsites: () => (fetchCampsites()),
-  resetFeedbackForm: () => (actions.reset('feedbackForm')),
-  fetchComments: () => (fetchComments()),
-  fetchPromotions: () => (fetchPromotions())
+  postComment: (campsiteId, rating, author, text) =>
+    postComment(campsiteId, rating, author, text),
+  fetchCampsites: () => fetchCampsites(),
+  resetFeedbackForm: () => actions.reset("feedbackForm"),
+  fetchComments: () => fetchComments(),
+  fetchPromotions: () => fetchPromotions(),
 };
 
 class Main extends Component {
-
   componentDidMount() {
     this.props.fetchCampsites();
     this.props.fetchComments();
     this.props.fetchPromotions();
-}
+  }
 
   render() {
     const HomePage = () => {
       return (
         <Home
           campsite={
-            this.props.campsites.campsites.filter((campsite) => campsite.featured)[0]
+            this.props.campsites.campsites.filter(
+              (campsite) => campsite.featured
+            )[0]
           }
           campsitesLoading={this.props.campsites.isLoading}
           campsitesErrMess={this.props.campsites.errMess}
           promotion={
-            this.props.promotions.promotions.filter((promotion) => promotion.featured)[0]
+            this.props.promotions.promotions.filter(
+              (promotion) => promotion.featured
+            )[0]
           }
           promotionLoading={this.props.promotions.isLoading}
           promotionErrMess={this.props.promotions.errMess}
@@ -65,9 +74,11 @@ class Main extends Component {
           }
           isLoading={this.props.campsites.isLoading}
           errMess={this.props.campsites.errMess}
-          comments={this.props.comments.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
+          comments={this.props.comments.comments.filter(
+            (comment) => comment.campsiteId === +match.params.campsiteId
+          )}
           commentsErrMess={this.props.comments.errMess}
-          addComment={this.props.addComment}
+          postComment={this.props.postComment}
         />
       );
     };
@@ -88,7 +99,13 @@ class Main extends Component {
             path="/aboutus"
             render={() => <About partners={this.props.partners} />}
           />
-          <Route exact path='/contactus' render={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
+          <Route
+            exact
+            path="/contactus"
+            render={() => (
+              <Contact resetFeedbackForm={this.props.resetFeedbackForm} />
+            )}
+          />
           <Redirect to="/home" />
         </Switch>
         <Footer />
